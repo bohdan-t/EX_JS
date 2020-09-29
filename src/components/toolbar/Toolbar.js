2167c99d6bd26c15c84d10d5375d49835a7c5bdd
Bohdan <tutejschei@gmail.com>
2020-09-29 14:16:19 +0300

    Add function to remove events listener

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -2,6 +2,13 @@ import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
+
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click']
+    });
+  }
   toHTML() {
     return ` <div class="button">
                     <span class="material-icons">
@@ -33,5 +40,9 @@ export class Toolbar extends ExcelComponent {
                         format_underline
                     </span>
             </div>`
+  }
+
+  onClick(event) {
+    console.log(event.target)
   }
 }
